32b6abe765b74c140b31488daa53dfd0d1c4d66c
Chad Xing <Da.Xing@microsoft.com>
2021-11-07 00:27:01 +0800

    Package JAR

--- src/watch/doge/Main.kt
+++ src/watch/doge/Main.kt
@@ -3,20 +3,24 @@ package watch.doge
 import java.util.*
 import kotlin.concurrent.fixedRateTimer
 
-fun main() {
-    var price = fetchPrice()
-    notifyOnce(price)
-    lastPrice = price
-    val minute = Calendar.getInstance().get(Calendar.MINUTE)
-    val nextMinute = when (minute) {
-        in (0 until 15) -> 15
-        in (15 until 30) -> 30
-        in (30 until 45) -> 45
-        else -> 60
-    }
-    fixedRateTimer(initialDelay = (nextMinute - minute).toLong() * 60 * 1000, period = 15.toLong() * 60 * 1000) {
-        price = fetchPrice()
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        var price = fetchPrice()
         notifyOnce(price)
         lastPrice = price
+        val minute = Calendar.getInstance().get(Calendar.MINUTE)
+        val nextMinute = when (minute) {
+            in (0 until 15) -> 15
+            in (15 until 30) -> 30
+            in (30 until 45) -> 45
+            else -> 60
+        }
+        fixedRateTimer(initialDelay = (nextMinute - minute).toLong() * 60 * 1000, period = 15.toLong() * 60 * 1000) {
+            price = fetchPrice()
+            notifyOnce(price)
+            lastPrice = price
+        }
     }
 }
