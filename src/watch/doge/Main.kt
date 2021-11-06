a9f98e98def7508c030619bf00f07639f641b13f
Chad Xing <Da.Xing@microsoft.com>
2021-11-07 00:11:52 +0800

    A working version

--- src/watch/doge/Main.kt
+++ src/watch/doge/Main.kt
@@ -1,5 +1,22 @@
 package watch.doge
 
-fun main() {
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
+fun main() {
+    var price = fetchPrice()
+    notifyOnce(price)
+    lastPrice = price
+    val minute = Calendar.getInstance().get(Calendar.MINUTE)
+    val nextMinute = when (minute) {
+        in (0 until 15) -> 15
+        in (15 until 30) -> 30
+        in (30 until 45) -> 45
+        else -> 60
+    }
+    fixedRateTimer(initialDelay = (nextMinute - minute).toLong() * 60 * 1000, period = 15.toLong() * 60 * 1000) {
+        price = fetchPrice()
+        notifyOnce(price)
+        lastPrice = price
+    }
 }
--- src/watch/doge/Utils.kt
+++ src/watch/doge/Utils.kt
@@ -8,26 +8,48 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+var lastPrice: Double? = null
+
 private var trayIcon: TrayIcon? = null
 
-fun fetchLast(): String {
+fun fetchPrice(): Double {
     val client = HttpClient.newHttpClient()
     val req = HttpRequest.newBuilder(URI.create("https://www.okex.com/api/index/v3/ETH-USDT/constituents")).build()
     val res = client.send(req, HttpResponse.BodyHandlers.ofString())
     val body =  res.body()
-    val last = extractLast(body)
-    return String.format("%.2f", last.toDouble())
+    val last = extractPrice(body)
+    return last.toDouble()
 }
 
-private fun extractLast(res: String): String {
+private fun extractPrice(res: String): String {
     val key = """"last":""""
     val start = res.indexOf(key) + key.length
     val end = res.indexOf('"', start)
     return res.substring(start, end)
 }
 
+fun notifyOnce(price: Double) {
+    val title = formatPrice(price)
+    var level = TrayIcon.MessageType.INFO
+    val desc: String
+    if (lastPrice != null) {
+        val changeInPercentage = (price - lastPrice!!) / lastPrice!!
+        if (changeInPercentage >= 0.02 || changeInPercentage <= -0.02) {
+            level = TrayIcon.MessageType.WARNING
+        }
+        val changeInDesc = (if (changeInPercentage >= 0) "上涨 " else "下跌 ") + formatPrice(changeInPercentage * 100) + '%'
+        desc = "ETH/USDT 在 15 分钟内$changeInDesc, 现报 $title, 前值 ${formatPrice(lastPrice!!)}" +
+                if (level == TrayIcon.MessageType.WARNING) ". 请注意控制风险." else ""
+    } else {
+        desc = "ETH/USDT 现报 $title"
+    }
+    notifyOnce(title, desc, level)
+}
+
+private fun formatPrice(price: Double) = String.format("%.2f", price)
+
 @Synchronized
-private fun notify(title: String, desc: String, level: TrayIcon.MessageType) {
+private fun notifyOnce(title: String, desc: String, level: TrayIcon.MessageType) {
     if (trayIcon == null) {
         if (!SystemTray.isSupported()) return
         val tray = SystemTray.getSystemTray()
